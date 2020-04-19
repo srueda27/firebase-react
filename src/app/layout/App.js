df54de5e51d6ffffa952e3cb1385fe708d634ba5
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-04-19 17:04:39 -0500

    add Semantic-ui-react

---
+++ src/app/layout/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Button} from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS Button
+        </button>
+        <Button icon="smile" content="React Button"/>
+      </div>
+    );
+  }
+}
+
+export default App;
