d5adbc2d0741d833a89370ec8e438cbe82a05b43
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-04-25 23:37:54 -0500

    add Redux configure store

---
+++ src/features/testArea/TestComponent.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { incrementCounter, decrementCounter } from './testAction';
+import { Button } from 'semantic-ui-react';
+
+const mapStateToProps = (state) => ({
+  data: state.test.data
+})
+
+const mapActionToProps = {
+  incrementCounter,
+  decrementCounter
+}
+
+class TestComponent extends Component {
+  render() {
+    const { data, incrementCounter, decrementCounter } = this.props;
+    return (
+      <div>
+        <h1>Test Component</h1>
+        <h3>The answer is: {data}</h3>
+        <Button
+          onClick={incrementCounter}
+          positive
+          content='Increment' />
+        <Button
+          onClick={decrementCounter}
+          negative
+          content='Decrement' />
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapActionToProps)(TestComponent);
